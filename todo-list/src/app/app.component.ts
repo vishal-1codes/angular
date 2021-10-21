8ec23cc27b718cefe047fde45fc01099192b326a
vishal-1codes <pawarvishal10k@gmail.com>
2021-10-21 22:01:48 +0530

    creating todo application inside todo angular, add all file to angular todo-list git repo

--- todo-list/src/app/app.component.ts
+++ todo-list/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  //here we create setTime function for change title after 2 second using constructor
   title = 'todo-list';
+  constructor(){
+  //   setTimeout(()=>{
+  //     this.title="Changed title"
+  //   },2000);
+  }
 }
