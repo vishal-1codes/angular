c6c3af4f54bc8aa30b8672a9cab555b5126d74f7
vishal-1codes <pawarvishal10k@gmail.com>
2021-10-24 20:27:54 +0530

    Adding all file of todos app in angular git repo

--- todo-list/src/app/MyComponents/todos/todos.component.ts
+++ todo-list/src/app/MyComponents/todos/todos.component.ts
@@ -1,13 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Todo} from '../../Todo'
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
+//here we todos:Todo[]; here it give error 
+//makes: any[] | undefined (also use this)
 
-  constructor() { }
+  todos?:Todo[];
+  constructor() {
+    this.todos=[
+      {
+        sno:1,
+        title:"this is title",
+        desc:"description",
+        active:true
+      },
+      {
+        sno:2,
+        title:"this is title 2",
+        desc:"description 2",
+        active:true
+      },
+      {
+        sno:3,
+        title:"this is title 3",
+        desc:"description 3",
+        active:true
+      }
+    ]
+   }
 
   ngOnInit(): void {
   }
