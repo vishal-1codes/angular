8832909737118a231d5512da9931d9cc3b11628d
vishal-1codes <pawarvishal10k@gmail.com>
2021-10-25 22:43:27 +0530

    add form in angular git repo

--- reactive-form/form/src/app/app.module.ts
+++ reactive-form/form/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormNgTask1Component } from './reactive-form-ng-task1/reactive-form-ng-task1.component';
+import { FormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { ReactiveFormNgTask1Component } from './reactive-form-ng-task1/reactive-
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
