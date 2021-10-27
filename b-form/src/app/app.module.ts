87ef8df3fa486ba1166640c2b224ab1f1c4e7909
vishal-1codes <pawarvishal10k@gmail.com>
2021-10-27 23:28:03 +0530

    adding b-form into angular github repo

--- b-form/src/app/app.module.ts
+++ b-form/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { InputSixWayComponent } from './input-six-way/input-six-way.component';
+import { FormsModule } from '@angular/forms';
+//#1 first import formsModule
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputSixWayComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
