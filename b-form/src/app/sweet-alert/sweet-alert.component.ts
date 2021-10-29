6b91310b5ef423d61ceced511fee27933833ed42
vishal-1codes <pawarvishal10k@gmail.com>
2021-10-29 21:17:12 +0530

    adding sweet alert in angular github repo

--- b-form/src/app/sweet-alert/sweet-alert.component.ts
+++ b-form/src/app/sweet-alert/sweet-alert.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+//here we import route
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-sweet-alert',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SweetAlertComponent implements OnInit {
 
-  constructor() { }
+  //here add route in _router
+  constructor(public _router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  //#sweeta here we add click event
+  //#sweeta here we add sweet alert for delete and file 
+  onClick(){
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      //we add success , error in icon value
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        //if we have to route this page we also route using router module
+        // this._router.navigate(["/l-one"]);
+        Swal.fire(
+          'Deleted!',
+          'Your file has been deleted.',
+          'success'
+        )
+      }
+    })
   }
 
 }
